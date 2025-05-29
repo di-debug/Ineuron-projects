6699e5087de3bcdadac799a45391ab460ec8bd88
di-debug <mrddust88@gmail.com>
2025-05-29 18:18:11 +0530

    Javascript Trim and Truthy Values

---
+++ Question08/index.js
@@ -0,0 +1,6 @@
+// 08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+const string = "   Hello, World!   ";
+const trimString = string.trim();
+console.log(`Original string: "${string}"`);
+console.log(`Trimmed string: "${trimString}"`);
---
+++ Question09/index.js
@@ -0,0 +1,15 @@
+// 09. Boolean value is either true or false.
+//     - Write three JavaScript statement example which provide truthy value.
+//     - Write three JavaScript statement example which provide falsy value.
+
+// Truthy values
+let truthyValue1 = 1; // Non-zero number
+let truthyValue2 = "Hello"; // Non-empty string
+let truthyValue3 = {}; // Non-empty object
+console.log(`Truthy values: ${truthyValue1}, ${truthyValue2}, ${truthyValue3}`);
+
+// Falsy values
+let falsyValue1 = 0; // Zero
+let falsyValue2 = ""; // Empty string
+let falsyValue3 = null; // Null value
+console.log(`Falsy values: ${falsyValue1}, ${falsyValue2}, ${falsyValue3}`);
